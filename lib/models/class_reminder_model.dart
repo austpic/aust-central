7bd33246860d9ed52b8ceaea4ddec7a7e03d9387
Arafat Akib <arafatakib100@gmail.com>
2026-08-05 16:22:59 +0600

    add course icon

--- lib/models/class_reminder_model.dart
+++ lib/models/class_reminder_model.dart
@@ -1,10 +1,14 @@
 class ClassReminderModel {
   final String courseName;
+  final String weekday;
+  final String classTime;
   bool isEnabled;
   int minutesBefore;
 
   ClassReminderModel({
     required this.courseName,
+    required this.weekday,
+    required this.classTime,
     this.isEnabled = true,
     this.minutesBefore = 10,
   });
--- lib/viewmodels/class_reminder_viewmodel.dart
+++ lib/viewmodels/class_reminder_viewmodel.dart
@@ -3,12 +3,12 @@ import '../models/class_reminder_model.dart';
 
 class ClassReminderViewModel extends ChangeNotifier {
   final List<ClassReminderModel> _reminders = [
-    ClassReminderModel(courseName: 'Data Structures', isEnabled: true, minutesBefore: 10),
-    ClassReminderModel(courseName: 'Database Systems', isEnabled: true, minutesBefore: 15),
-    ClassReminderModel(courseName: 'Digital Logic Design', isEnabled: false, minutesBefore: 5),
-    ClassReminderModel(courseName: 'Discrete Mathematics', isEnabled: true, minutesBefore: 10),
-    ClassReminderModel(courseName: 'English Composition', isEnabled: false, minutesBefore: 30),
-    ClassReminderModel(courseName: 'Physics Lab', isEnabled: true, minutesBefore: 15),
+    ClassReminderModel(courseName: 'Data Structures',weekday: 'Sunday', classTime: '10:00 AM',isEnabled: true, minutesBefore: 10),
+    ClassReminderModel(courseName: 'Database Systems', weekday: 'Tuesday',classTime: '1:00 PM', isEnabled: true, minutesBefore: 15),
+    ClassReminderModel(courseName: 'Digital Logic Design',weekday: 'Tuesday', classTime: '2:00 PM',isEnabled: false, minutesBefore: 5),
+    ClassReminderModel(courseName: 'Discrete Mathematics',weekday: 'Tuesday',classTime: '3:00 PM', isEnabled: true, minutesBefore: 10),
+    ClassReminderModel(courseName: 'English Composition',weekday: 'Monday',classTime: '4:00 PM', isEnabled: false, minutesBefore: 30),
+    ClassReminderModel(courseName: 'Physics Lab',weekday: 'Monday', classTime: '5:00 PM', isEnabled: true, minutesBefore: 15),
   ];
 
   bool _isSaved = false;
@@ -35,7 +35,24 @@ class ClassReminderViewModel extends ChangeNotifier {
     _isSaved = false;
     notifyListeners();
   }
+  void addReminder({
+  required String courseName,
+  required String weekday,
+  required String classTime,
+  int minutesBefore = 10,
+}) {
+  _reminders.add(
+    ClassReminderModel(
+      courseName: courseName,
+      weekday: weekday,
+      classTime: classTime,
+      minutesBefore: minutesBefore,
+    ),
+  );
 
+  _isSaved = false;
+  notifyListeners();
+}
   void saveSettings() {
     _isSaved = true;
     notifyListeners();
